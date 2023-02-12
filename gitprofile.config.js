4915b931e7b8f6a2836753d54174fd217a4b476a
Emmanuel Elias <31974716+mcjovial@users.noreply.github.com>
2023-02-12 04:18:39 +0100

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -215,7 +215,7 @@ const config = {
 
   // Optional Footer. Supports plain text or HTML.
   footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
+      class="text-primary" href="https://github.com/mcjovial.github.io"
       target="_blank"
       rel="noreferrer"
     >GitProfile</a> and ❤️`,
